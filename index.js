f168c88f9ca0f87e5b5a0800553904c0036006d7
Nazila Rahimi <153054137+nazila-rahimi@users.noreply.github.com>
2025-02-10 01:45:53 +0000

    updated

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const server = http.createServer((req, res) => {
     }
 }); 
 
-// Start the server safely
+
+// Start the server 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 }).on('error', (err) => {
